accea109108b74ed3a4f8e031e4c066c6bcac991
HadiaTariq2011 <hadiaasad2011@gmail.com>
2025-05-12 15:00:29 +0500

    Add files via upload

---
+++ statment.py
@@ -0,0 +1,17 @@
+print("welcome to the world of programming")
+print(4)
+print("hello python \n")
+print("hello", 5)
+print("welcome to", end="*")
+x=5
+y="sam"
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+name=input("enter your name")
+print("\n hello", name, "\n welcome to codingal")
+import keyword
+# all the python keywords are
+print("python keywords are...\n")
+print(keyword.kwlist)
