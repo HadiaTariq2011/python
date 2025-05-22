1d93bd7a6ac1f58ce4e715382d8c4fbc7f003b90
HadiaTariq2011 <hadiaasad2011@gmail.com>
2025-05-22 15:46:50 +0500

    Add files via upload

---
+++ conditional statments.py
@@ -0,0 +1,36 @@
+num=3
+if num > 0:
+    print(num,"is a positive number")
+num=-1
+if num > 0:
+    print(num,"is a positive number")
+
+actual_cost = float (input("please enter actual amount"))
+sales_cost = float (input("please enter sales amount"))
+if (sales_cost > actual_cost):
+        amount = sales_cost - actual_cost
+        print("total profit = {0}".format(amount))
+else:
+        print("no profitt!!!")
+
+
+number =int(input("enter number to check:"))
+print("number to be checked:", number)
+if number%2==0:  
+     print("number is even")
+else:
+      print("number is odd ")
+
+
+i = int(input("enter a number:"))
+if (i < 15):
+      print("i is smaller than 15")
+      print("i am in i block")
+else:
+      print("i ia greater than 15")
+      print("i am not in i block ")   
+print("i am not in if not than else")   
+    
+        
+
+
