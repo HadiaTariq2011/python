11d8539a3519a93560387082155a71de26c2482a
HadiaTariq2011 <hadiaasad2011@gmail.com>
2025-05-31 15:11:49 +0500

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,23 @@
+n=int(input("enter number whose sum you want " ))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+    print("\n sum=",sum) 
+
+
+
+string=input("plz enter your string:"  )
+string2=('')
+for i in string:
+    string2= i + string2
+print("\n original string", string)
+print("\nreverced string", string2)
+
+
+n=int(input("enter the number : "))
+print("numbers from {0} to{1} are".format(n,1))
+for i in range (n,0,-1):
+    print(i)
+
+
+             
