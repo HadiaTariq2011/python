3076ea6ff769bf3784aa1048c95e00b255da4cfa
HadiaTariq2011 <hadiaasad2011@gmail.com>
2025-06-26 15:55:46 +0500

    Add files via upload

---
+++ break.py
@@ -0,0 +1,27 @@
+a=input("enter a word:")
+for i in a:
+    if (i=='a'):
+     print("A is found")
+     break
+    else:
+       print("A not found") 
+
+
+
+
+for x in range(10):
+   if x % 20==0:
+      print("twist")
+   elif x % 15==0:
+      pass
+   elif x % 20==0:
+      print("fuzz")
+   elif x % 20==0:
+      print("buzz")
+   else:
+      print(x)  
+      
+      
+    
+
+        
---
+++ pass1.py
@@ -0,0 +1,24 @@
+
+for x in range(10):
+   if x%20==0:
+      print("twist")
+   elif x%15==0:
+      pass
+   elif x%5==0:
+      print("fizz")
+   elif x%3==0:
+      print("buzz")
+   else:
+      print(x) 
+
+
+
+
+
+var=10
+while var>0:
+   var=var-1
+   if var==5:
+      continue
+   print("\ncurrent variable value:",var)
+print("\n good bye")
