7afacd24ce48a64b59eedaf79debd2e85c058d48
HadiaTariq2011 <hadiaasad2011@gmail.com>
2025-07-12 15:22:00 +0500

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,12 +1,45 @@
-def well_wishes():
-    print("hello")
-    print("how are u?")
-well_wishes()  
+tuplex=("tuple",False,3.2,1)
+print(tuplex)
+tuplex=(1,2,3,4,5,6,7)
+print(tuplex)
+tuplex=tuplex+(9,)
+print(tuplex)
+tuplex=(1,2,3,4,7,8,9,6)
+_slice=tuplex[3:5]
+print(_slice)
+_slice=tuplex[:6]
+print(_slice)
 
 
-def weather_condition():
-    print('weather is pleasent in:',spring)
-    print('weather is same in ',autumn)
-    spring="autmn"
-    autumn=spring
-    weather_condition()
+
+def palind(r):
+    e = len(r)-1
+    s=0
+    while s<e:
+        if(r[s]!=r[e]):
+            return False
+        s+=1
+        e-=1
+        return True
+r= (1,2,3,3,2,1)
+if(palind(r)):
+    print("tuple is flip flop")
+else:
+    print("not flip flop")
+
+
+weather=(1,0,0,1,0,1,0)
+sunny=0
+rainy=0
+for i in range(0,7):
+    if (weather[i]==0):
+        rainy+=1
+    else:
+        sunny+=1
+if (sunny>rainy):
+        print("good weather")
+else:
+        print("bad weather")    
+        
+
+
