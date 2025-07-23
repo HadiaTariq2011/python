a4ab417c197810adea4bfb29611fddce4db0c55b
HadiaTariq2011 <hadiaasad2011@gmail.com>
2025-07-23 11:41:41 +0500

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,89 +1,30 @@
-theBoard={'7':'','8':'','9':'',
-        '4':'','5':'','6':'',
-        '1':'','2':'','3':''}
-board_keys=[]
-for key in theBoard:
-    board_keys.append(key)
-def printBoard(board):
-    print(board['7']+'|'+board['8']+'|'+board['9'])
-    print('-+-+-')    
-    print(board['4']+'|'+board['5']+'|'+board['6'])
-    print('-+-+-')
-    print(board['1']+'|'+board['2']+'|'+board['3'])
-    print('-+-+-')  
+class vehicle:
+    def __init__(self,max_speed,mileage):
+      self.max_speed= max_speed
+      self.mileage= mileage
+modelX=vehicle(240,18) 
+print("model max speed",modelX.max_speed)
+print("model mileage",modelX.mileage)
 
-def game():
-    turn="X"
-    count=0
-    for i in range(10):
-        printBoard(theBoard)
-        print("It is ur turn,"+turn+".Move to which place")
-        move=input()
-        if theBoard[move]=='':
-            theBoard[move]=turn
-            count+=1 
-        else:
-            print("the space is already filled")
-            continue       
-        if count  >=5:
-            if theBoard['7']==theBoard['8']==theBoard['9']!='':
-                printBoard(theBoard)
-                print("\nGame Over.\n")
-                print("****"+turn+"won.****")
-                break
-            elif theBoard['4']==theBoard['5']==theBoard['6']!='':
-                printBoard(theBoard)
-                print("\nGame Over.\n")
-                print("****"+turn+"won.****")
-                break
-            elif theBoard['1']==theBoard['2']==theBoard['3']!='':
-                printBoard(theBoard)
-                print("\nGame Over.\n")
-                print("****"+turn+"won.****")
-                break
-            elif theBoard['1']==theBoard['4']==theBoard['7']!='':
-                printBoard(theBoard)
-                print("\nGame Over.\n")
-                print("****"+turn+"won.****")
-                break
-            elif theBoard['2']==theBoard['5']==theBoard['8']!='':
-                printBoard(theBoard)
-                print("\nGame Over.\n")
-                print("****"+turn+"won.****")
-                break
-            elif theBoard['3']==theBoard['6']==theBoard['9']!='':
-                printBoard(theBoard)
-                print("\nGame Over.\n")
-                print("****"+turn+"won.****")
-                break
-            elif theBoard['7']==theBoard['5']==theBoard['3']!='':
-                printBoard(theBoard)
-                print("\nGame Over.\n")
-                print("****"+turn+"won.****")
-                break
-            elif theBoard['1']==theBoard['5']==theBoard['9']!='':
-                printBoard(theBoard)
-                print("\nGame Over.\n")
-                print("****"+turn+"won.****")
-                break
-        if count ==9:
-            print("\nGame over\n its a tie")
-            if turn=="X":
-                turn="0"
-            else:
-                turn="X"
-    restart=input("Do u want to play again?y/n")
-    if restart=="Y"or restart=="y":
-        for key in board_keys:
-             theBoard  [key]=""
-        game()
-if __name__=="__main__":
-    game()             
 
-        
+
+class parrot:
+   species="bird"
+   def __init__(self,name,age):
+      self.name=name
+      self.age=age
+blu=parrot("blu","10")
+woo=parrot("woo","15")
+print("blue is a{}".format(blu.species))
+print("blue is a{}".format(woo.species))
+print("{}is{}years old",format(blu.name,blu.age))
+print("{}is{}years old",format(woo.name,woo.age))
 
 
 
 
 
-    
+
+
+
+      
