9b1005ed6bbd213ac69c0034100ca2fd62f10efa
HadiaTariq2011 <hadiaasad2011@gmail.com>
2025-07-26 15:12:30 +0500

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,22 +1,13 @@
-def hotel_cost(nights):
-    return 140*nights
-def plane_ride_cost(city):
-    if "islamabad"==city:
-        return 183
-    elif "tampa"==city:
-        return 220
-    elif "pittsburgh"==city:
-        return 222
-    elif "los Angeles"==city:
-        return 457
-def rental_car_cost(days):
-    if days>=7:
-        return 40*days-50
-    elif days>=3:
-        return 40*days-20
-    else:
-        return 40* days
-    
-def trip_cost(city,days,spending_money):
-    return rental_car_cost(days)+hotel_cost(days)+plane_ride_cost(city)
-print("total cost of the trip:",trip_cost( "los Angeles",7,500))
+class Employee:
+    def __init__(self):
+       print("employee created")
+    def __del__ (self):
+       print("destructor called")
+def create_obj():
+   print('making object...')
+   obj=Employee()
+   print('function end.....')
+   return obj
+print('calling create_obj() function...')
+obj=create_obj()
+print("end programe")
