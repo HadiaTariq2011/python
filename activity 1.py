dbb4a550dc9a4569a429d55e6b956dc97add5032
HadiaTariq2011 <hadiaasad2011@gmail.com>
2025-07-27 14:17:48 +0500

    Add files via upload

--- activity 1.py
+++ activity 1.py
@@ -1,10 +1,27 @@
-class IOString():
-    def __init__(self):
-        self.str1=""
-    def get_string(self):
-        self.str1=input("enter string:")
-    def print_string(self):
-        print("result is :",self.str1.upper())
-str1=IOString()
-str1.get_string()
-str1.print_string()
+class Vehicle:
+    def __init__(self,name,max_speed,mileage):
+        self.name=name
+        self.max_speed=max_speed
+        self.mileage=mileage
+class Bus(Vehicle):
+        pass
+School_bus=Bus("School Volvo",180,12) 
+print("vehicle name:",School_bus.name,"speed",School_bus.max_speed,"Mileage",School_bus.mileage )
+
+
+
+
+class person(object):
+     def __init__(self,idnumber,name):
+          self.name=name
+          self.idnumber=idnumber
+     def display(self):
+           print(self.name)
+           print(self.idnumber)
+class Employee(person):
+      def __init__(self,name,idnumber,salary,post):
+           self.salary=salary
+           self.post=post 
+           super().__init__(idnumber,name)   
+a=Employee('Rahul',886012,2000000,"intern")
+a.display()                          
