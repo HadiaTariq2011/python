e2d9b2dc96cbda6fb7608db5866346700aaf2756
HadiaTariq2011 <hadiaasad2011@gmail.com>
2025-08-16 15:23:45 +0500

    Add files via upload

--- activity 3.py
+++ activity 3.py
@@ -1,9 +1,10 @@
-class pair_elements:
-    def twoSum(self,nums,target):
-        lookup={}
-        for i, num in enumerate(nums):
-            if target - num in lookup:
-                return (lookup[target-num],1)
-            lookup[num]=i
-value=int(input("enter sum for which you want to make this search:"))
-print("index1=%d,index2=%d"% pair_elements().twoSum((10,20,30,40,50,60,70),value))            
+import pygame
+pygame.init()
+screen=pygame.display.set_mode((400,300))
+done=False
+while not done:
+    for event in pygame.event.get():
+      if event.type==pygame.QUIT:
+        done=True
+    pygame.draw.rect(screen,(0,125,255),pygame.Rect(30,30,60,60))
+    pygame.display.flip()
--- activity2.py
+++ activity2.py
@@ -1,13 +1,19 @@
-class Employee:
-    def __init__(self):
-       print("employee created")
-    def __del__ (self):
-       print("destructor called")
-def create_obj():
-   print('making object...')
-   obj=Employee()
-   print('function end.....')
-   return obj
-print('calling create_obj() function...')
-obj=create_obj()
-print("end programe")
+import pygame
+pygame.init()
+SCREEN_WIDTH,SCREEN_HEIGHT=500,500
+display_surface=pygame.dispaly.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.dispaly.set_caption('adding imageand background image')
+background_image=pygame.transform.scale(
+    pygame.image.load('background.png').convrt(),
+    (SCREEN_WIDTH,SCREEN_HEIGHT))
+penguin_image=pygame.transform.scale(
+    pygame.image.load('hello penguin.png').convert_alpha(),(200,200)
+penguin_react=penguin_imge.get_react(center=(SCREEN_WIDTH//2,
+                                             SCREEN_HEIGHT//2-30 ))
+text=pygame.font.Font(None,360).render('hello world ',True,
+                                        pygame.coor('black'))
+text_rect=text.get_rect(center=(SCREEN_WIDTH//2,SCREEN_HEIGHT//2+110)))
+def game_loop():
+    clock
+
+
